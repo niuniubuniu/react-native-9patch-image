0d98fa6bdab856d77a210b1db20dc65dc44ae399
牛康康 <1261103246@qq.com>
2019-08-15 15:33:50 +0800

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -29,7 +29,9 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     bottom: 0,
-    right: 0
+    right: 0,
+    width: '100%',
+    height: '100%',
   }
 });
 export default NinePatchView;
