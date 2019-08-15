0f8b8fab7f11e7d5b129f343eecf51dc1b930071
牛康康 <1261103246@qq.com>
2019-08-15 15:15:46 +0800

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import {
   Image,
+  StyleSheet,
 } from 'react-native';
 
 
 class NinePatchView extends Component {
   render() {
+    const { source, children, rest} = this.props
     return (
-      <Image
-        {...this.props}
-        resizeMode='stretch'
-      />
+      <View {...rest}>
+        <Image
+          source={source}
+          style={styles.capInset}
+          resizeMode='stretch'
+        />
+        {children}
+      </View>
     );
   }
 }
-
+const styles = StyleSheet.create({
+  capInset: {
+    resizeMode: 'stretch',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0
+  }
+});
 export default NinePatchView;
