ca69906071d7a6674ab89ec43ff2da9d36a34b80
牛康康 <1261103246@qq.com>
2019-09-25 15:17:59 +0800

    Update RCTImageCache.java

--- android/src/main/java/su/rusfearuth/reactnative/native9patch/RCTImageCache.java
+++ android/src/main/java/su/rusfearuth/reactnative/native9patch/RCTImageCache.java
@@ -1,7 +1,7 @@
 package su.rusfearuth.reactnative.native9patch;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
